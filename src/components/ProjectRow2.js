5906fc60a7ec859a76cc6a2e70650d09f17dbdc6
D Marcus <mindlesscorporatedrone@gmail.com>
2021-05-10 14:34:24 -0700

    added ProjectSmall placeholders

--- src/components/ProjectRow2.js
+++ src/components/ProjectRow2.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import ProjectSmall from './ProjectSmall'
+import './_ProjectRow2.scss'
 
 function ProjectRow2() {
     return (
         <div className="ProjectRow2">
-            
+            <div className="ProjectRow2-group">
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+            </div>
+            <div className="ProjectRow2-group">
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+                <ProjectSmall sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Test App'} />
+            </div>
+
         </div>
     )
 }
--- src/components/ProjectSmall.js
+++ src/components/ProjectSmall.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './_ProjectSmall.scss'
 
-function ProjectSmall() {
+function ProjectSmall(props) {
+    const { sourceImg, title, description } = props;
     return (
-        <div>
-            
+        <div className="ProjectSmall">
+            <div className="ProjectSmall-main">
+                <img src={sourceImg}></img>
+            </div>
+            <div className="ProjectSmall-view">
+                <p>{title}</p>
+                <div className="ProjectSmall-buttons">
+                    <button>Github</button>
+                    <button>Live</button>
+                </div>
+            </div>
         </div>
     )
 }
