9b484a27ab27be12312722fb2b709911693b6acc
D Marcus <mindlesscorporatedrone@gmail.com>
2021-05-11 20:45:23 -0700

    aligned about icons

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import './_Contact.scss'
 function Contact() {
     return (
         <div className="Contact" id="contact">
-            <h2>Get In Touch</h2>
+            <h1>Get In Touch</h1>
             <p>Feel free to reach out if you're looking for a developer, have a question, or just want to connect.</p>
             <a href="mailto:webmaster@example.com">Jon Doe</a>
         </div>
