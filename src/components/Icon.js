5e6819c4d586a0854538a6b0015a5d96e019d622
D Marcus <mindlesscorporatedrone@gmail.com>
2021-05-13 15:09:47 -0700

    added react-tooltip for about section

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import ReactTooltip from "react-tooltip";
 import './_Icon.scss'
 
 function Icon(props) {
-    const { name } = props;
+    const { name, tipName, tipText, about } = props;
 
     return (
-        <i className={name}></i>
+        <>
+            <i className={name} data-tip data-for={tipName}></i>
+            {about && <ReactTooltip id={tipName} place="top" effect="solid">
+                {tipText}
+            </ReactTooltip>}
+        </>
     )
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -13,20 +13,20 @@ function About() {
             </p>
                 <div className="About-icons">
                     <div className="About-icons-group-1">
-                        <Icon name={'fab fa-html5 fa-4x'} />
-                        <Icon name={'fab fa-css3-alt fa-4x'} />
-                        <Icon name={'fab fa-js-square fa-4x'} />
-                        <Icon name={'fab fa-sass fa-4x'} />
+                        <Icon name={'fab fa-html5 fa-4x'} tipName={'htmlTip'} tipText={'HTML'} about={true}/>
+                        <Icon name={'fab fa-css3-alt fa-4x'} tipName={'cssTip'} tipText={'CSS'} about={true}/>
+                        <Icon name={'fab fa-js-square fa-4x'} tipName={'jsTip'} tipText={'Javascript'} about={true}/>
+                        <Icon name={'fab fa-sass fa-4x'} tipName={'sassTip'} tipText={'Sass'} about={true}/>
                     </div>
                     <div className="About-icons-group-2">
-                        <Icon name={'fab fa-bootstrap fa-4x'} />
-                        <Icon name={'fab fa-react fa-4x'} />
-                        <Icon name={'fab fa-git-alt fa-4x'} />
-                        <Icon name={'fab fa-github fa-4x'} />
+                        <Icon name={'fab fa-bootstrap fa-4x'} tipName={'bootstrapTip'} tipText={'Bootstrap'} about={true}/>
+                        <Icon name={'fab fa-react fa-4x'} tipName={'reactTip'} tipText={'React'} about={true}/>
+                        <Icon name={'fab fa-git-alt fa-4x'} tipName={'gitTip'} tipText={'Git'} about={true}/>
+                        <Icon name={'fab fa-github fa-4x'} tipName={'githubTip'} tipText={'Github'} about={true}/>
                     </div>
                     <div className="About-icons-group-3">
-                        <Icon name={'fab fa-figma fa-4x'} />
-                        <Icon name={'fab fa-node fa-4x'} />
+                        <Icon name={'fab fa-figma fa-4x'} tipName={'figmaTip'} tipText={'Figma'} about={true}/>
+                        <Icon name={'fab fa-node fa-4x'} tipName={'nodeTip'} tipText={'Node.js'} about={true}/>
                     </div>
                 </div>
             </Fade>
