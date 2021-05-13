e8da533193234724d2e76c841c5e5c06052f3bfd
D Marcus <mindlesscorporatedrone@gmail.com>
2021-05-13 13:57:44 -0700

    added tech info to project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link} from 'react-router-dom';
+
 import './_Project.scss'
 
 function Project(props) {
-    const { sourceImg, title, description } = props;
+    const { sourceImg, title, description, technology } = props;
+
+    const techItems = technology.map(item => (
+        <div className="Project-technology-item">{item}</div>
+    ))
+
     return (
         <div className="Project">
             <img src={sourceImg}></img>
             <h2>{title}</h2>
             <p>{description}</p>
+            <div className="Project-technology">
+                {techItems}
+            </div>
+            <div className="Project-buttons">
+                <button>Github</button>
+                <button>Live</button>
+            </div>
         </div>
     )
 }
--- src/components/ProjectRow1.js
+++ src/components/ProjectRow1.js
@@ -3,12 +3,27 @@ import Project from './Project'
 import './_ProjectRow1.scss'
 
 function ProjectRow1() {
+
+    const Chirp = {
+        sourceImg: 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',
+        title: 'Chirp',
+        description: 'A 7 day weather application that sets weather data based on user input.',
+        technology: ['MongoDB', 'Express', 'React', 'Node.js', 'Passport', 'Cloudinary']
+    }
+
+    const Devblog = {
+        sourceImg: 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',
+        title: 'Devblog',
+        description: 'A 7 day weather application that sets weather data based on user input.',
+        technology: ['MongoDB', 'Express', 'EJS', 'Node.js', 'Passport', 'Cloudinary', 'Bootstrap', 'TinyMCE']
+    }
+
     return (
         <div className="ProjectRow1">
             <h1>Projects</h1>
             <div className="ProjectRow1-projects">
-                <Project sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Chirp'} description={'A 7 day weather application that sets weather data based on user input.'} />
-                <Project sourceImg={'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'} title={'Devblog'} description={'A 7 day weather application that sets weather data based on user input.'} />
+                <Project sourceImg={Chirp.sourceImg} title={Chirp.title} description={Chirp.description} technology={Chirp.technology} />
+                <Project sourceImg={Devblog.sourceImg} title={Devblog.title} description={Devblog.description} technology={Devblog.technology} />
             </div>
 
         </div>
